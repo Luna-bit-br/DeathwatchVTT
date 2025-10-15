593ed0b30514cc00ef3a4b4881765b15442569c6
Luna-bit-br <pedroograndemicrocamp@gmail.com>
2025-10-15 00:24:37 -0300

    Update item-sheet.js

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -1,58 +1,19 @@
 /**
- * Extend the basic ItemSheet with some very simple modifications
- * @extends {ItemSheet}
+ * Sheet personalizada para Itens
  */
 export class DeathwatchItemSheet extends ItemSheet {
-
-  /** @override */
   static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
+    return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["deathwatch", "sheet", "item"],
+      template: "systems/deathwatch/templates/item-sheet.hbs",
       width: 520,
-      height: 480,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+      height: 480
     });
   }
 
-  /** @override */
-  get template() {
-    const path = "systems/deathwatch/templates/item";
-    // Return a single sheet for all item types.
-    return `${path}/item-sheet.html`;
-    // Alternatively, you could use the following return statement to do a
-    // unique item sheet by type, like `weapon-sheet.html`.
-
-    // return `${path}/${this.item.data.type}-sheet.html`;
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
   getData() {
     const data = super.getData();
+    data.config = CONFIG.Deathwatch;
     return data;
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  setPosition(options = {}) {
-    const position = super.setPosition(options);
-    const sheetBody = this.element.find(".sheet-body");
-    const bodyHeight = position.height - 192;
-    sheetBody.css("height", bodyHeight);
-    return position;
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
-
-    // Roll handlers, click handlers, etc. would go here.
   }
 }

401d5db547b1c03f7e2e7ee0bfe1ef5723c18455
Luna-bit-br <pedroograndemicrocamp@gmail.com>
2025-10-15 00:23:40 -0300

    Update actor.js

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -1,37 +1,17 @@
 /**
- * Extend the base Actor entity by defining a custom roll data structure which is ideal for the Simple system.
- * @extends {Actor}
+ * Classe base de Actor (personagem, NPC, etc.)
  */
 export class DeathwatchActor extends Actor {
+  /** Dados derivados e automáticos */
+  prepareDerivedData() {
+    const data = this.system;
 
-  /**
-   * Augment the basic actor data with additional dynamic data.
-   */
-  prepareData() {
-    super.prepareData();
+    // Exemplo de cálculo de ferimentos com base em Toughness
+    if (data.stats?.t?.value) {
+      data.wounds.max = 10 + data.stats.t.value;
+    }
 
-    const actorData = this.data;
-    const data = actorData.data;
-    const flags = actorData.flags;
-
-    // Make separate methods for each Actor type (character, npc, etc.) to keep
-    // things organized.
-    if (actorData.type === 'character') this._prepareCharacterData(actorData);
+    // Prevenir erros de campos inexistentes
+    data.wounds.value = data.wounds.value ?? data.wounds.max;
   }
-
-  /**
-   * Prepare Character type specific data
-   */
-  _prepareCharacterData(actorData) {
-    const data = actorData.data;
-
-    // Make modifications to data here. For example:
-
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    // for (let [key, ability] of Object.entries(data.abilities)) {
-      // Calculate the modifier using d20 rules.
-      // ability.mod = Math.floor((ability.value - 10) / 2);
-    // }
-  }
-
 }
